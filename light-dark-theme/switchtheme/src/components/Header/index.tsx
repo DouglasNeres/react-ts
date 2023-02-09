ae1ff1acbc66a24be7483cfbecdd0cbc4f14a3e6
DouglasNeres <douglasneres06@gmail.com>
2023-02-08 23:12:13 -0300

    Tsx - Switcher Light and Dark Theme with React Ts and Styled components.

---
+++ light-dark-theme/switchtheme/src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React, {useContext} from 'react';
+import { Container } from './styles';
+import Switch from 'react-switch'
+import { ThemeContext } from 'styled-components'
+import { shade } from 'polished'
+
+interface Props {
+  toggleTheme(): void
+}
+
+export const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const {colors, title} = useContext(ThemeContext)
+
+  return (
+    <Container>
+      Hello World!
+
+      <Switch 
+      onChange={toggleTheme}
+      checked={title === 'dark'}
+      checkedIcon={false}
+      uncheckedIcon={false}
+      height={10}
+      width={40}
+      handleDiameter={20}
+      offColor={shade(0.15, colors.primary)}
+      onColor={colors.secundary}
+
+      />
+    </Container>
+  )
+
+}
