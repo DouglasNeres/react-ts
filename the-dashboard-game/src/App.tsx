332566fd1d38cdb2c4b4495660b5e27bf58ddff5
DouglasNeres <douglasneres06@gmail.com>
2023-06-16 21:35:01 -0300

    Rename folders and project modified

--- the-dashboard-game/src/App.tsx
+++ the-dashboard-game/src/App.tsx
@@ -2,10 +2,12 @@ import { GameList } from './components/GameList'
 import { Header } from './components/Header'
 import { MainBanner } from './components/MainBanner'
 import { MyGamesList } from './components/MyGameList'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Header />
       <MainBanner />
       <GameList />
---
+++ the-dashboard-game/src/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { MdDehaze, MdHome } from "react-icons/md";
+import { StyledSidebar } from "./styles";
+
+export const Sidebar = () => {
+  return (
+    <StyledSidebar>
+      <MdDehaze />
+      <MdHome />
+    </StyledSidebar>
+  );
+};
